e72a2793aa1cdeceee06827752ff6a89b53c2412
arhamhmulani7podar-pixel <arhamhmulani7.podar@gmail.com>
2026-03-05 23:35:31 +0530

    fix: Remove legacy API folder causing deployment errors

--- api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.handler = async (event) => {
-  // Your serverless function logic here
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from serverless!' }),
-  };
-};
